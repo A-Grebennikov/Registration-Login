bc3710f73efcfa235f1346dc94557d32aca76798
agrebennikov <agrebennikov.exceedteam@gmail.com>
2019-11-26 11:24:01 +0300

    changed classNames

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -37,14 +37,14 @@ export default class Profile extends React.Component {
 
   renderUsernames() {
     return this.state.usernames.map(item => (
-      <div className="Main">
-        <div className="logo">
-          <img className="logoProfile" src="http://s1.iconbird.com/ico/2013/11/504/w128h1281385326502profle.png" />
+      <div className="profile-users">
+        <div>
+          <img className="logo-profile" src="http://s1.iconbird.com/ico/2013/11/504/w128h1281385326502profle.png" />
         </div>
-        <div className="data">
-          <span className="name" key={createID("name")}>{item.name}</span>
-          <div className="email">
-            <img className="logoEmail" src="https://cdn.icon-icons.com/icons2/788/PNG/512/email_icon-icons.com_64925.png" />
+        <div className="users-data">
+          <span className="users-data__name" key={createID("name")}>{item.name}</span>
+          <div className="users-data__email">
+            <img className="logo-email" src="https://cdn.icon-icons.com/icons2/788/PNG/512/email_icon-icons.com_64925.png" />
             <span> {item.email}</span>
           </div>
         </div>
@@ -55,8 +55,8 @@ export default class Profile extends React.Component {
   render() {
     return (
       <div className="userList-container">
-        <h1 className="headerList"> List of registered Users</h1>
-        <div className="userList-list">
+        <h1 className="header-list"> List of registered Users</h1>
+        <div className="userList__list">
           {this.renderUsernames()}
         </div>
         <LogoutButton onClick={this.handleLogoutClick} />
@@ -67,7 +67,7 @@ export default class Profile extends React.Component {
 
 function LogoutButton(props) {
   return (
-    <button onClick={props.onClick} className="logout-button">
+    <button onClick={props.onClick} className="logout_button">
       Logout
     </button>
   );
